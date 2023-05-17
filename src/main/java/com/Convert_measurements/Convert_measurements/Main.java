961025c6b8bc4f20030f9d2e962bfc93d2946595
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-17 16:14:37 +0400

    add 'z' condition

--- src/main/java/com/Convert_measurements/Convert_measurements/Main.java
+++ src/main/java/com/Convert_measurements/Convert_measurements/Main.java
@@ -10,7 +10,7 @@ public class Main {
         char[] alphabet = "_abcdefghijklmnopqrstuvwxyz".toCharArray();
         Map<Character, Integer> letterNumberMap = new HashMap<>();
 
-        for (int i = 0; i < alphabet.length; i++){
+        for (int i = 0; i < alphabet.length; i++) {
             char letter = alphabet[i];
             int number = i;
             letterNumberMap.put(letter, number);
@@ -21,34 +21,31 @@ public class Main {
 
         StringBuilder extractedWord = new StringBuilder();
 
-        boolean isZHandled = false; // Flag to track if 'z' has been handled as a special case
-
-        while (inputWord.length() > 1){
+        while (inputWord.length() > 1) {
             char firstLetter = inputWord.charAt(0);
             int number = letterNumberMap.getOrDefault(firstLetter, -1);
 
-            if(number != -1){
-                if(inputWord.length() > 1 && inputWord.length() >= number+1){
-                        String subWord = inputWord.substring(1, number + 1);
-                        extractedWord.append(subWord).append(" ");
-                        inputWord = inputWord.substring(number + 1);
-                }
-                else{
-                    //extractedWord.append(inputWord);
+            if (number != -1) {
+                if (inputWord.length() > 1 && inputWord.length() >= number + 1) {
+                    String subWord = inputWord.substring(1, number + 1);
+                    extractedWord.append(subWord).append("");
+                    inputWord = inputWord.substring(number + 1);
+
+                    if (subWord.contains("z")) {
+                        if (inputWord.length() > 0) {
+                            extractedWord.append(inputWord.charAt(0)).append("");
+                            inputWord = inputWord.substring(1);
+                        }
+                    }
+                    extractedWord.append(" ");
+                } else {
                     extractedWord.append(0);
                     inputWord = "";
                 }
-            }
-            else{
+            } else {
                 inputWord = inputWord.substring(1);
             }
         }
-        /*System.out.println(inputWord.substring(0+1,0+2));
-        for (int i = 0; i<inputWord.length(); i++){
-            int val = letterNumberMap.get(inputWord.charAt(i));
-            extractedWord.append(inputWord.substring(i+1,i+val+1));
-            i = i+val;
-        }*/
 
         System.out.println("Extracted Subwords: " + extractedWord);
     }
